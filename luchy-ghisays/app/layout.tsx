c2f61e95cfb75f7c0d28fc9b5dbfb0296410c5c5
Marcosuniaga1 <masglobaltrade.1@gmail.com>
2026-08-15 15:57:06 -0400

    fix: correct Meta Pixel ID to 750814929609134

--- luchy-ghisays/app/layout.tsx
+++ luchy-ghisays/app/layout.tsx
@@ -49,14 +49,14 @@ export default function RootLayout({
             n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
             t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
             (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init','283637846626618');
+            fbq('init','750814929609134');
             fbq('track','PageView');
           `}
         </Script>
         <noscript>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img height="1" width="1" style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=283637846626618&ev=PageView&noscript=1" alt="" />
+            src="https://www.facebook.com/tr?id=750814929609134&ev=PageView&noscript=1" alt="" />
         </noscript>
 
         {/* Google Analytics 4 */}
